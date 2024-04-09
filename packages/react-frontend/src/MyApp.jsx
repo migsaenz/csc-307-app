578bd21dfa69e132c0484f4d5fe399b4a0b7a153
Miguel Saenz <saenzm4307@gmail.com>
2024-04-09 10:44:05 -0700

    Final Commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -11,13 +11,15 @@ function removeOneCharacter(index) {
     });
     setCharacters(updated);
   }  
-
+function updateList(person){
+	setCharacters([...characters, person]);
+  }
 return (
     <div className="container">
       <Table characterData={characters} 
 	removeCharacter={removeOneCharacter}
 	/>
-      <Form />
+      <Form handleSubmit={updateList}/>
     </div>
   );
 }
